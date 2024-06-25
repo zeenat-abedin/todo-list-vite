64d5d9c4bdf73e17996b4d531731f2aa9936d8c2
zeenat <zeenu.23@gmail.com>
2024-06-25 19:54:37 +0530

    remove useState import and count state variable in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Todo List</h1>
       <div className="todo-list">
-        
+
       </div>
     </>
   )
