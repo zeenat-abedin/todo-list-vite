07204ace73e906ed33d87fe4bb849a81cee0af02
zeenat <zeenu.23@gmail.com>
2024-06-25 21:17:13 +0530

    add todo list functionality,
    and modified index.css to update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [data, setData] = useState("")
+  const [todoList, setTodoList] = useState([])
+  
+  function handleTodo(e) {
+    e.preventDefault()
+    setData(e.target.value)
+  }
+
+  function handleSubmit() {
+    if (data.trim !== "") {     
+      setTodoList([...todoList, data])
+      setData("")
+    }
+  }
 
   return (
-    <>
+    <div className='todo-list'>
       <h1>Todo List</h1>
-      <div className="todo-list">
-
-      </div>
-    </>
+      <input value={data} onChange={handleTodo} />
+      <button onClick={handleSubmit}>Add Todo</button>
+      <ul className='list-item'>
+        {todoList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
