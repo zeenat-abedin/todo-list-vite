37ad2b5737644f072bc1c683d4c47cb77b2e7d6a
zeenat <zeenu.23@gmail.com>
2024-06-25 19:54:00 +0530

    initialize

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Todo List</h1>
+      <div className="todo-list">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
